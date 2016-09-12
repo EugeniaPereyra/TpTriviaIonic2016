680b842518d4729362d2a7a128331cc99b72e347
Eugenia <eugenia>
2016-09-12 13:35:35 -0300

    juego trivia

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,11 +10,77 @@ angular.module('starter.controllers', [])
 	}
 })
 
-.controller('TriviaCtrl', function($scope, $stateParams) {
+.controller('TriviaCtrl', function($scope, $timeout, $stateParams) {
 
 	var nombre=JSON.parse($stateParams.nombre);
   	$scope.usuario={};
+  	$scope.usuario.puntaje=0;
   	$scope.usuario.nombre=nombre.nombre;
+
+  	var preguntas=new Firebase('https://trivia-ce4e1.firebaseio.com/preguntas');
+  	$scope.preguntas=[];
+
+  	preguntas.on('child_added', function (snapshot){
+  		$timeout(function(){
+  			var pregunta=snapshot.val();
+  			$scope.preguntas.push(pregunta);
+  			//console.log($scope.preguntas);
+  		});
+  	});
+
+  	$scope.Verificar=function(opcion,pregunta){
+  		if(pregunta.id==1)
+  		{
+	  		if(opcion==pregunta.respuesta)
+	  		{
+	  			alert("Correcto!!");
+	  			$scope.usuario.pregunta1="bien";
+	  			$scope.usuario.puntaje+=pregunta.puntos;
+	  		}
+	  		else
+	  		{
+	  			alert("incorrecto");
+	  			$scope.usuario.pregunta1="mal";
+	  		}
+  		}
+  		if(pregunta.id==2)
+  		{
+	  		if(opcion==pregunta.respuesta)
+	  		{
+	  			alert("Correcto!!");
+	  			$scope.usuario.pregunta2="bien";
+	  			$scope.usuario.puntaje+=pregunta.puntos;
+	  		}
+	  		else
+	  		{
+	  			alert("incorrecto");
+	  			$scope.usuario.pregunta2="mal";
+	  		}
+  		}
+  	  	if(pregunta.id==3)
+  		{
+	  		if(opcion==pregunta.respuesta)
+	  		{
+	  			alert("Correcto!!");
+	  			$scope.usuario.pregunta3="bien";
+	  			$scope.usuario.puntaje+=pregunta.puntos;
+	  		}
+	  		else
+	  		{
+	  			alert("incorrecto");
+	  			$scope.usuario.pregunta3="mal";
+	  		}
+  		}
+  	}
+
+  	var usuarios=new Firebase('https://trivia-ce4e1.firebaseio.com/usuarios');
+  	$scope.Salir=function(){
+  		usuario.push({usuario:$scope.usuario.nombre, 
+  						pregunta1:$scope.usuario.pregunta1,
+  						pregunta2:$scope.usuario.pregunta2,
+  						pregunta3:$scope.usuario.pregunta3,
+  						puntaje:$scope.usuario.puntaje });
+  	}
 
 })
 
